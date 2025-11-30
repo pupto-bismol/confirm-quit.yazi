094ff2127dfbd09cc05645acb58f73643b6f56e4
maxyw <maxy.ww@pm.me>
2025-11-29 22:22:36 -0800

    extend functionality with option for --close arg

--- main.lua
+++ main.lua
@@ -1,18 +1,33 @@
-local count = ya.sync(function() return #cx.tabs end)
+local M = {}
 
-local function entry()
+local count = ya.sync(function() return #cx.tabs end)
+local no_cwd_file = job.args.no_cwd_file
+local close = job.args.close
+
+local function quit_or_close()
+	if close then return "close" else return "quit" end
+end
+
+function M:entry()
 	if count() < 2 then
-		return ya.emit("quit", {})
+		return ya.emit(quit_or_close(), {no_cwd_file})
+	end
+
+	local title = ""
+	if close then
+		title = "Close?",
+	else
+		title = "Quit?",
 	end
 
 	local yes = ya.confirm {
 		pos = { "center", w = 60, h = 10 },
-		title = "Quit?",
+		title = title,
 		content = ui.Text("There are multiple tabs open. Are you sure you want to quit?"):wrap(ui.Wrap.YES),
 	}
 	if yes then
-		ya.emit("quit", {})
+		ya.emit(quit_or_close(), {no_cwd_file})
 	end
 end
 
-return { entry = entry }
+return M
